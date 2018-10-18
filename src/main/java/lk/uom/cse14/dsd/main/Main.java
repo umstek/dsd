4a451fd3c20b1e837abfc03de95539fb59d27dc7
Wickramaranga Abeygunawardhana <umstek@live.com>
2018-10-18 17:06:01 +0530

    Add valid hosts filter

--- src/main/java/lk/uom/cse14/dsd/main/Main.java
+++ src/main/java/lk/uom/cse14/dsd/main/Main.java
@@ -3,26 +3,37 @@ package lk.uom.cse14.dsd.main;
 import lk.uom.cse14.dsd.bscom.PeerInfo;
 import lk.uom.cse14.dsd.bscom.RegisterException;
 import lk.uom.cse14.dsd.bscom.TcpRegistryCommunicator;
+import lk.uom.cse14.dsd.util.NetworkInterfaceUtils;
 
 import java.io.IOException;
+import java.net.SocketException;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        TcpRegistryCommunicator x = new TcpRegistryCommunicator("127.0.0.1", 5000);
+//        TcpRegistryCommunicator x = new TcpRegistryCommunicator("127.0.0.1", 5000);
+//        try {
+//            List<PeerInfo> peerInfos = x.register("127.0.0.1", 4990, "user1");
+//            for (PeerInfo pi : peerInfos) {
+//                System.out.println(pi);
+//            }
+//        } catch (IOException e) {
+//            System.out.println("IO error");
+//            e.printStackTrace();
+//        } catch (RegisterException e) {
+//            System.out.println("Registering error");
+//            e.printStackTrace();
+//        }
+
         try {
-            List<PeerInfo> peerInfos = x.register("127.0.0.1", 4990, "user1");
-            for (PeerInfo pi : peerInfos) {
-                System.out.println(pi);
+            List<String> ownHosts = NetworkInterfaceUtils.findOwnHosts(false);
+
+            for (String ownHost : ownHosts) {
+                System.out.println(ownHost);
             }
-        } catch (IOException e) {
-            System.out.println("IO error");
-            e.printStackTrace();
-        } catch (RegisterException e) {
-            System.out.println("Registering error");
+        } catch (SocketException e) {
             e.printStackTrace();
         }
-
     }
 }
--- src/main/java/lk/uom/cse14/dsd/util/NetworkInterfaceUtils.java
+++ src/main/java/lk/uom/cse14/dsd/util/NetworkInterfaceUtils.java
@@ -1,25 +1,46 @@
 package lk.uom.cse14.dsd.util;
 
+import java.net.Inet6Address;
+import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
+import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.List;
 
 public class NetworkInterfaceUtils {
-    public static String findOwnHosts() throws SocketException {
+    public static List<String> findOwnHosts(boolean allowLoopback) throws SocketException {
         Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+
+        ArrayList<String> validHosts = new ArrayList<>();
+
         while (networkInterfaces.hasMoreElements()) {
             NetworkInterface networkInterface = networkInterfaces.nextElement();
 
-            /* We want to connect to somewhere external. */
-            // TODO Add networkInterface.isLoopback() || networkInterface.isVirtual() || in production.
             if (!networkInterface.isUp()) {
                 continue;
             }
+            /* We want to connect to somewhere external unless we're in dev mode. */
+            if (!allowLoopback) {
+                if (networkInterface.isLoopback() || networkInterface.isVirtual()) {
+                    continue;
+                }
+            }
 
-            // TODO Find where we can successfully start a node.
+            Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
+            while (inetAddresses.hasMoreElements()) {
+                InetAddress inetAddress = inetAddresses.nextElement();
+
+                /* Filter-out IPV6 Addresses. */
+                if (inetAddress instanceof Inet6Address) {
+                    continue;
+                }
+
+                String hostAddress = inetAddress.getHostAddress();
+                validHosts.add(hostAddress);
+            }
         }
 
-        // TODO Return
-        return null;
+        return validHosts;
     }
 }
