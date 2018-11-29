423e13a6fe017b78fc14fc3c73a8ced53c2b3a4c
isuruperera <tharanga.14@cse.mrt.ac.lk>
2018-11-30 02:22:48 +0530

    Log4j logs implementation

--- src/main/java/lk/uom/cse14/dsd/comm/response/QueryResponse.java
+++ src/main/java/lk/uom/cse14/dsd/comm/response/QueryResponse.java
@@ -1,5 +1,6 @@
 package lk.uom.cse14.dsd.comm.response;
 
+import lk.uom.cse14.dsd.comm.MessageType;
 import lk.uom.cse14.dsd.msghandler.QueryResultSet;
 
 public class QueryResponse extends Response {
@@ -7,6 +8,7 @@ public class QueryResponse extends Response {
 
     public QueryResponse(String source, int sourcePort, String destination, int destinationPort) {
         super(source, sourcePort, destination, destinationPort);
+        this.setType(MessageType.QUERY);
     }
 
     public QueryResultSet getQueryResultSet() {
