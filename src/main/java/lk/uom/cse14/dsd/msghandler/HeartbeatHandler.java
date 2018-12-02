704218d0df5d249731bd8a880edcb65142bae2d9
isuruperera <tharanga.14@cse.mrt.ac.lk>
2018-12-02 15:10:30 +0530

    HB Handler updates

--- src/main/java/lk/uom/cse14/dsd/msghandler/HeartbeatHandler.java
+++ src/main/java/lk/uom/cse14/dsd/msghandler/HeartbeatHandler.java
@@ -136,41 +136,25 @@ public class HeartbeatHandler implements IHandler, Runnable {
     public void run() {
         while (true) {
             try{
-                boolean flag1 = false;
-                boolean flag2 = false;
-                int entryCount = 0;
                 synchronized (RoutingEntry.class){
                     try {
-                        entryCount = routingEntries.size();
-                        if (entryCount < 1) {
-                            flag1 = true;
-                        } else {
+                        int entryCount = routingEntries.size();
+                        if (entryCount > 1) {
                             for (RoutingEntry routingEntry : routingEntries) {
                                 HeartbeatRequest heartbeatRequest = new HeartbeatRequest(
                                         ownHost, ownPort, routingEntry.getPeerIP(), routingEntry.getPeerPort()
                                 );
                                 scheduler.schedule(heartbeatRequest);
-                                flag2 = true;
                             }
                         }
                     }catch (Exception e){
                         e.printStackTrace();
                     }
                 }
-                if(flag1){
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                if(flag2){
-                    try {
-                        Thread.sleep(10000 / entryCount);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }catch (Exception e){
                 e.printStackTrace();
